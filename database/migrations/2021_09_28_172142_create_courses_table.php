c67d18096e306c5490e26b11727de08976e658ab
reynaldsauro <naldsauro@gmail.com>
2021-09-28 10:31:33 -0700

    week6

---
+++ database/migrations/2021_09_28_172142_create_courses_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCoursesTable extends Migration
+{
+    /*
+     * Run the migrations.
+     *
+     * @return void
+    */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->string('description');
+            $table->bigInteger('trainor_id')->unsigned();
+            $table->timestamps();
+            $table->foreign('trainor_id')->references('id')->on('trainors');
+        });
+    }
+
+    /*
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+}
